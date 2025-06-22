fd5c23febdcb9935212c50edcfc3b23736c66cca
Sara <sarailic431@gmail.com>
2025-06-22 23:58:39 +0200

    ...

--- src/CoctailItem.jsx
+++ src/CoctailItem.jsx
@@ -4,10 +4,12 @@ function CoctailItem(props) {
   const { image, name, id } = props;
   // console.log(id);
 
-  function showIngredients(id) {
+  function showIngredients(name) {
     const fetchPost = async () => {
       const response = await fetch(
-        `https://thecocktaildb.com/api/json/v1/1/lookup.php?iid=${id}`
+        // `https://thecocktaildb.com/api/json/v1/1/lookup.php?iid=${id}`
+        // `https://thecocktaildb.com/api/json/v1/1/lookup.php?s=${name}`
+        `https://thecocktaildb.com/api/json/v1/1/search.php?s=${name}`
       );
       const posts = await response.json();
       console.log(posts);
@@ -16,15 +18,15 @@ function CoctailItem(props) {
     fetchPost();
   }
   return (
-    <article className="coctail-item" onClick={() => showIngredients(id)}>
+    <article className="coctail-item" onClick={() => showIngredients(name)}>
       <span className="image-cont">
         {" "}
         <img src={image} className="coctail-img" />{" "}
       </span>
       <h5> {name} </h5>
 
-      <div class="cart-button-empty">
-        <i class="bx bx-cart-add">
+      <div className="cart-button-empty">
+        <i className="bx bx-cart-add">
           {" "}
           <span className="add-to-chart-text">Add to Cart</span>
         </i>
